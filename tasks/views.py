66f82a531fca65d4de2a4f1302cca749ae04630d
Ajluna <albertojlg94@gmail.com>
2024-02-16 16:34:49 -0400

    base.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,32 +1,42 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+from django.contrib.auth import login 
+from django.db  import IntegrityError
 
 # Create your views here.
+
+
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def signup(request):
 
-    if request.method == 'GET': 
+    if request.method == 'GET':
         return render(request, 'signup.html', {
-             'form' : UserCreationForm
-  })
-    
+            'form': UserCreationForm
+        })
+
     else:
-      if request.POST['password1'] == request.POST['password1']:
-         try: 
-            user = User.objects.create_user(username=request.POST['username'],
-            password=request.POST['password1'])
-            user.save()
-            return HttpResponse('Usuario creado satifactoriamente')
-         except:
-            return HttpResponse ('El usuario ya existe')
-      return HttpResponse('Password do not match')
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],password=request.POST
+                    ['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+               return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error": 'El susuario ya existe'
+                })
 
+        return render(request, 'signup.html', {
+                'form': UserCreationForm,
+                "error": 'Contraseña no coincide'
+        })
 
-
-
-      
-
+def tasks(request):
+       return render(request, 'tasks.html')
