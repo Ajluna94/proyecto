09cafca80f92beedb3aa0159af2f8c6e6bc19dc8
Ajluna <albertojlg94@gmail.com>
2024-02-29 10:17:38 -0400

    agregando bustrap

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.db  import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required 
 
 # Create your views here.
 
@@ -39,12 +41,22 @@ def signup(request):
                 "error": 'Contraseña no coincide'
         })
 
+@login_required 
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {
         'tasks': tasks
     })
 
+@login_required 
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by
+    ('-datecompleted')
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
+
+@login_required 
 def create_task(request):
 
    if request.method == 'GET': 
@@ -63,7 +75,8 @@ def create_task(request):
                 'form': TaskForm,
                  'error': 'Please provide valida data'
             })
-      
+
+@login_required       
 def task_detail(request, task_id):
     if request.method == 'GET': 
        task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -74,11 +87,27 @@ def task_detail(request, task_id):
          task = get_object_or_404(Task, pk=task_id, user=request.user) 
          form = TaskForm(request.POST, instance=task)
          form.save()
-         return redirect('task')
+         return redirect('tasks')
        except ValueError:
            return render(request, 'task_detail.html', {'task': task, 'form': form ,
           'error': "error updating task" })
-           
+
+@login_required 
+def  complete_task(request, task_id): 
+   task = get_object_or_404(Task, pk=task_id, user=request.user) 
+   if request.method == 'POST':
+       task.datecomplete = timezone.now()
+       task.save()
+       return redirect('tasks')
+
+@login_required 
+def  delete_task(request, task_id): 
+   task = get_object_or_404(Task, pk=task_id, user=request.user) 
+   if request.method == 'POST':
+       task.delete()
+       return redirect('tasks')
+
+@login_required 
 def signout(request):
     logout(request) 
     return redirect('home') 
